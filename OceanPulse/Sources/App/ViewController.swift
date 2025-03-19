b22a340d14a33b2779d006942e5dc29c59b6ae85
fassy363 <fassy363@gmail.com>
2025-03-19 17:17:55 +0200

    add isues

--- OceanPulse/Sources/App/ViewController.swift
+++ OceanPulse/Sources/App/ViewController.swift
@@ -6,20 +6,61 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let onboardingScreen = FirstOnboardingScreen(viewModel: .init())
-        let hostingController = UIHostingController(rootView: onboardingScreen)
+        let onbScreen = FourthOnboardingScreen(path: .constant(.init()))
+        let hostContr = UIHostingController(rootView: onbScreen)
         
-        addChild(hostingController)
-        view.addSubview(hostingController.view)
-        hostingController.didMove(toParent: self)
+        addChild(hostContr)
+        view.addSubview(hostContr.view)
+        hostContr.didMove(toParent: self)
         
-        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        hostContr.view.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
-            hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            hostContr.view.topAnchor.constraint(equalTo: view.topAnchor),
+            hostContr.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            hostContr.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            hostContr.view.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
+    }
+    
+    func firstOpen() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            let onboardingScreen = FirstOnboardingScreen(viewModel: .init())
+            let hostingController = UIHostingController(rootView: onboardingScreen)
+            self.rootViewC(hostingController)
+        }
+    }
+    
+    func secondOpen(string: String) {
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            for child in self.children {
+                if child is HelpViewController {
+                    return
+                }
+            }
+            guard !string.isEmpty else { return }
+            let secondController = HelpViewController(url: string)
+            self.addChild(secondController)
+            secondController.view.frame = self.view.bounds
+            secondController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            self.view.addSubview(secondController.view)
+            secondController.didMove(toParent: self)
+        }
+    }
+
+    
+    func rootViewC(_ viewController: UIViewController) {
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+            appDelegate.window?.rootViewController = viewController
+        }
+    }
+    
+    func helpStr(mainSTR: String, devID: String, advertID: String, appsId: String) -> (String) {
+        var str = ""
+        
+        str = "\(mainSTR)?xxcx=\(devID)&adlb=\(advertID)&rbvy=\(appsId)"
+        
+        return str
     }
 }
 
